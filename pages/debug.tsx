3d372f1a1c7906a9d10613c7d16d92565f24339b
Dick Wyn Yong <dickwyn@outlook.com>
2022-07-22 20:18:30 -0700

    (feat) setup core routes

---
+++ pages/debug.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+import styles from '../styles/Home.module.css';
+
+const Debug: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <h1>debug</h1>
+    </div>
+  );
+};
+
+export default Debug;
---
+++ pages/uses.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+import styles from '../styles/Home.module.css';
+
+const Uses: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <h1>uses</h1>
+    </div>
+  );
+};
+
+export default Uses;
