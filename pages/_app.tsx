b89920b11f4be274c66d0fd805e5cf2efb5c8521
Dick Wyn Yong <dickwyn@outlook.com>
2022-07-24 20:18:07 -0700

    (feat) add google analytics plugin

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import '../styles/globals.scss';
-
+import { GoogleAnalytics } from 'nextjs-google-analytics';
 import type { AppProps } from 'next/app';
 
 const DwyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   const AnyComponent = Component as any;
-  return <AnyComponent {...pageProps} />;
+  return (
+    <>
+      <GoogleAnalytics />
+      <AnyComponent {...pageProps} />
+    </>
+  );
 };
 
 export default DwyApp;
