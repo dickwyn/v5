dfe6d0722cab1e4ffdd0020b44c4a3cfd4a63657
Dick Wyn Yong <dickwyn@outlook.com>
2022-07-31 19:30:51 -0700

    (chore) cleanup base meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
 import '../styles/globals.scss';
 import { GoogleAnalytics } from 'nextjs-google-analytics';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 const DwyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   const AnyComponent = Component as any;
   return (
     <>
+      <Head>
+        {/* adding this here because https://github.com/vercel/next.js/blob/deprecated-main/errors/no-document-viewport-meta.md */}
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+      </Head>
       <GoogleAnalytics />
       <AnyComponent {...pageProps} />
     </>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,29 +16,30 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-          />
+          {/* google fonts */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-            rel="stylesheet"
-          />
           <link
             href="https://fonts.googleapis.com/css2?family=Exo+2:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&family=IBM+Plex+Mono&family=IBM+Plex+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500&family=IBM+Plex+Serif:ital@0;1&display=swap"
             rel="stylesheet"
           />
+          {/* mui */}
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
           <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+          {/* modern-favicons */}
           <link rel="icon" href="/favicon-48.png" type="image/png" />
           <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
           <link rel="apple-touch-icon" href="/favicon-apple-touch.png" />
           <link rel="manifest" href="/manifest.json" />
         </Head>
         <body>
+          {/* as recommended by library https://github.com/transitive-bullshit/use-dark-mode#nextjs */}
+          {/* eslint-disable-next-line @next/next/no-sync-scripts */}
           <script src="noflash.js" />
           <Main />
           <NextScript />
