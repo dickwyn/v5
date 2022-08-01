7a32286d3de707541ef593dfc509ca5e1aca19cc
Dick Wyn Yong <dickwyn@outlook.com>
2022-07-31 20:04:48 -0700

    (feat) add links to debug page

--- pages/debug.tsx
+++ pages/debug.tsx
@@ -1,8 +1,22 @@
 import Layout from '../components/layout';
+import StandardLink from '../components/standardLink';
 
 const DebugPage = (): JSX.Element => (
   <Layout title="Debug">
     <h1>debug</h1>
+    <h2>Diagnostic information:</h2>
+    <p>tbd</p>
+    <h2>Links:</h2>
+    <ul>
+      <li>
+        <span aria-label="information" role="img">
+          💅
+        </span>
+        <p>
+          <StandardLink href="/styleguide" label="View" /> the style guide.
+        </p>
+      </li>
+    </ul>
   </Layout>
 );
 
