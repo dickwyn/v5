4671a216759430b1d660569a01762a155aa8bb19
Dick Wyn Yong <dickwyn@outlook.com>
2022-08-02 23:16:35 -0700

    (feat) update content and style homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { GetStaticProps } from 'next';
 import Image from 'next/image';
 
+import styles from '../styles/index.module.scss';
 import Layout from '../components/layout';
 
 const IndexPage = (): JSX.Element => (
@@ -13,13 +14,19 @@ const IndexPage = (): JSX.Element => (
         height={600}
       />
     </div>
-    <p>software engineer at Microsoft</p>
-    <p>front end engineer at Clairvoyant/EXL; student at ASU</p>
-    <p>
-      my weekly schedule minus sleeping comprises 20% coding, 10% meetings, 10% cooking, 30% video
-      production, 25% learning
+    <h1 className={`${styles.title} ${styles.newSection}`}>hello, i&apos;m dickwyn</h1>
+    <h2 className={styles.subtitle}>
+      a software engineer that&apos;s passionate about content creation
+    </h2>
+    <p className={styles.newSection}>current: software engineer at Microsoft</p>
+    <p>previous: front end engineer at Clairvoyant/EXL; student at ASU</p>
+    <p className={`${styles.mutedText} ${styles.newSection}`}>
+      i like consumer technology, airplanes, photography and eating + cooking chicken rice
     </p>
-    <p>i like consumer technology, airplanes, photography and eating + cooking chicken rice</p>
+    <p className={styles.mutedText}>
+      time allocation: 20% coding, 20% youtube video making,10% meetings, 5% cooking, 15% learning,
+      30% sleep
+    </p>
   </Layout>
 );
 
