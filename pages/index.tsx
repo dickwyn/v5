84df5119d7c60a8e0d810bcef5cc10a5b850a76e
Dick Wyn Yong <dickwyn@outlook.com>
2022-08-13 00:09:31 -0700

    (style) update content and look+feel of homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import styles from '../styles/index.module.scss';
 import Layout from '../components/layout';
 import HeroImage from '../images/IMG_6764@0.5x.jpg';
+import StandardLink from '../components/standardLink';
 
 const IndexPage = (): JSX.Element => (
   <Layout>
@@ -17,12 +18,18 @@ const IndexPage = (): JSX.Element => (
         objectPosition="0 54%"
       />
     </div>
-    <h1 className={`${styles.title} ${styles.newSection}`}>hello, i&apos;m dickwyn</h1>
-    <h2 className={styles.subtitle}>
+    <h1 className={`${styles.title} ${styles.newSection} ${styles.minimalHeading}`}>
+      hello, i&apos;m dickwyn
+    </h1>
+    <h2 className={`${styles.subtitle} ${styles.minimalHeading}`}>
       a software engineer that&apos;s passionate about content creation
     </h2>
     <p className={styles.newSection}>current: software engineer at Microsoft</p>
-    <p>previous: front end engineer at Clairvoyant/EXL; student at ASU</p>
+    <p>
+      previous: front end engineering at{' '}
+      <StandardLink href="https://www.clairvoyant.ai/" label="Clairvoyant/EXL" />
+      ; computer science at <StandardLink href="https://scai.engineering.asu.edu/" label="ASU" />
+    </p>
     <p className={`${styles.mutedText} ${styles.newSection}`}>
       i like consumer technology, airplanes, photography, and eating + cooking chicken rice
     </p>
