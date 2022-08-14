e03d393d205c8c60ca49cbd5283b9aad28cd6802
Dick Wyn Yong <dickwyn@outlook.com>
2022-08-13 23:53:50 -0700

    (feat) Initial slug page for things page

--- lib/things.ts
+++ lib/things.ts
@@ -4,11 +4,11 @@ import fs from 'fs';
 import matter from 'gray-matter';
 import path, { join } from 'path';
 
-export const POSTS_PATH = path.join(process.cwd(), 'content', 'blog');
+export const THINGS_PATH = path.join(process.cwd(), 'content', 'things');
 
 export const getPostBySlug = (postPath: string) => {
     const realSlug = postPath.replace(/\.mdx$/, '');
-    const fullPath = join(POSTS_PATH, `${realSlug}.mdx`);
+    const fullPath = join(THINGS_PATH, `${realSlug}.mdx`);
     const { content, data } = matter(fs.readFileSync(fullPath, 'utf8'));
 
     return { content, frontMatter: { ...data, date: data.timestamp, slug: realSlug } };
@@ -16,7 +16,7 @@ export const getPostBySlug = (postPath: string) => {
 
 export const getAllPosts = (): any =>
     fs
-        .readdirSync(POSTS_PATH)
+        .readdirSync(THINGS_PATH)
         // TODO filter out fields that are needed
         .map((postPath) => getPostBySlug(postPath).frontMatter)
         .sort(({ date: a }, { date: b }) => (a > b ? -1 : 1));
