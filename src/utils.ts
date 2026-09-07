5549231cd4987586308f7036447624bae7dd7ae1
dickwyn <dickwyn@outlook.com>
2026-09-06 21:44:29 -0700

    refactor: extract shared post list component

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import getReadingTime from 'reading-time';
+
 const dateFormatter = new Intl.DateTimeFormat('en-US', {
     month: 'long',
     day: 'numeric',
@@ -7,3 +9,5 @@ const dateFormatter = new Intl.DateTimeFormat('en-US', {
 
 export const formatDate = (date: string) =>
     dateFormatter.format(new Date(date.includes('T') ? date : `${date}T00:00:00Z`));
+
+export const readingTime = (body: string) => getReadingTime(body).text;
