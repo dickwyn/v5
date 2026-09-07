f456fab9500c8a0ee521ef2937a02136450d4c17
dickwyn <dickwyn@outlook.com>
2026-09-06 21:42:54 -0700

    refactor: simplify date formatting

--- src/utils.ts
+++ src/utils.ts
@@ -1,36 +1,9 @@
-export const formatDate = (date: string, includeRelative = false) => {
-    const currentDate = new Date();
+const dateFormatter = new Intl.DateTimeFormat('en-US', {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+    timeZone: 'UTC',
+});
 
-    if (!date.includes('T')) {
-        date = `${date}T00:00:00`;
-    }
-    const targetDate = new Date(date);
-
-    const yearsAgo = currentDate.getFullYear() - targetDate.getFullYear();
-    const monthsAgo = currentDate.getMonth() - targetDate.getMonth();
-    const daysAgo = currentDate.getDate() - targetDate.getDate();
-
-    let formattedDate = '';
-
-    if (yearsAgo > 0) {
-        formattedDate = `${yearsAgo}y ago`;
-    } else if (monthsAgo > 0) {
-        formattedDate = `${monthsAgo}mo ago`;
-    } else if (daysAgo > 0) {
-        formattedDate = `${daysAgo}d ago`;
-    } else {
-        formattedDate = 'Today';
-    }
-
-    const fullDate = targetDate.toLocaleString('en-us', {
-        month: 'long',
-        day: 'numeric',
-        year: 'numeric',
-    });
-
-    if (!includeRelative) {
-        return fullDate;
-    }
-
-    return `${fullDate} (${formattedDate})`;
-};
+export const formatDate = (date: string) =>
+    dateFormatter.format(new Date(date.includes('T') ? date : `${date}T00:00:00Z`));
